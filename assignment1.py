5b75ab9e3266312f0bb4dd1d06b85979c1352b8b
Callum Gedling <callum.gedling@my.jcu.edu.au>
2019-08-27 11:43:18 +1000

    Add basic information to top doc string

--- assignment1.py
+++ assignment1.py
@@ -1,14 +1,14 @@
 """
 Replace the contents of this module docstring with your own details
-Name:
-Date started:
-GitHub URL:
+Name: Callum Gedling
+Date started: 27/8/19
+GitHub URL: https://github.com/cp1404-students/2019-2-a1-callumgedling
 """
 
 
 def main():
     """..."""
-    print("Movies To Watch 1.0 - by <Your Name>")
+    print("Movies To Watch 1.0 - by Callum Gedling")
 
 
 if __name__ == '__main__':
